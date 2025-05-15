4b1aa4fd886d0ea67106c1a3c8489b2b99472dc9
Marcoindrajaya21 <marco.indrajaya21@gmail.com>
2025-05-15 16:02:10 +0700

    pertemuan 11 a

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+
+<br>
+<a href="pertama">Pertemuan 1</a><br>
+
+
+</body>
+</html>
