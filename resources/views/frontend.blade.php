5b3ba244def4162d29f6d9e2a483d8752c6a7225
Marcoindrajaya21 <marco.indrajaya21@gmail.com>
2025-05-21 22:51:48 +0700

    PR PERTEMUAN 11

---
+++ resources/views/frontend.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Halaman Pertemuan</title>
+</head>
+<body>
+    <h1>Pemrograman WEB</h1>
+    <ul>
+         <li><a href="{{ url('/Index') }}">Index ETS</a></li>
+         <li><a href="{{ url('js1') }}">Index</a></li>
+         <li><a href="{{ url('/contact') }}">JS1</a></li>
+         <li><a href="{{ url('/pertama') }}">pertama</a></li>
+         <li><a href="{{ url('/tugaslogin') }}">Pertemuan 4</a></li>
+         <li><a href="{{ url('/validasi1') }}">Template 1</a></li>
+         <li><a href="{{ url('/linktree') }}">Tugas Linktree</a></li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,58 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// import java.io;
+use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 
-// System.out.println("Hello World");
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend');
 });
 
-Route::get('halo', function () {
-	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+
+Route::get('/blog', function () {
+    return view('blog');
 });
 
-Route::get('blog', function () {
-	return view('blog');
+Route::get('/Index', function () {
+    return view('Index');
 });
 
-Route::get('pertama', function () {
-	return view('pertama');
+Route::get('/js1', function () {
+    return view('js1');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/pertama', function () {
+    return view('pertama');
+});
+
+Route::get('/tugaslogin', function () {
+    return view('tugaslogin');
+});
+
+Route::get('/validasi1', function () {
+    return view('validasi1');
+});
+
+Route::get('/linktree', function () {
+    return view('linktree');
+});
+
+
+
