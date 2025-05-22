c798eda385340243a57f49c6c7fe03fba089f5a1
Marcoindrajaya21 <marco.indrajaya21@gmail.com>
2025-05-22 15:49:58 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Link;
+
+use App\Http\Controllers\DosenController;
+
 use App\Http\Controllers\PegawaiController;
+
 use App\Http\Controllers\BlogController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +22,12 @@ use App\Http\Controllers\BlogController;
 */
 
 
-Route::get('/', function () {
+Route ::get('/frontend', function () {
     return view('frontend');
 });
 
 
-Route::get('/blog', function () {
+Route::get('/blog' , function () {
     return view('blog');
 });
 
@@ -53,6 +58,26 @@ Route::get('/validasi1', function () {
 Route::get('/linktree', function () {
     return view('linktree');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+
+
+
+Route::get('welcome', [DosenController::class, 'welcome']);
+
+
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+
 
 
 
